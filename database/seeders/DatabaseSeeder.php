c87a469c53294d2b5a4290011ec1133b82a40a70
Alberts <ipa22.a.zibrevs@vtdt.edu.lv>
2024-10-22 10:29:39 +0300

    cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,30 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-        User::factory()->create([
-            "name"=> "admin@admin.admin",
-            "email"=> "admin@admin.admin",
-            "password"=> bcrypt("admin@admin.admin"),
-        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-=======
-=======
-        
->>>>>>> 748472978bba2c5a02d891256e1881f59c7cea44
+
+
         \App\Models\User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'role' => 'admin',
             'password' => 'admin',
         ]);
->>>>>>> 9d0b646858d90dc53453305f9f3cbf5afbd20773
     }
 }
